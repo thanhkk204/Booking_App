ec293c874d28adee1bda9b08c66744a2354bd21c
thanhkk204 <saibattu3kk@gmail.com>
2024-03-05 17:35:25 +0700

    initialize project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import './app.css'
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600">Lê huy thanh</h1>
+      <h1 className="text-primaryColor">
+        Hello word
+      </h1>
+    </>
+  )
+}
+
+export default App
