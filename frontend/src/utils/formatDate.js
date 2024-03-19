af1ffc57ee246c03d7cad18cae509e6678ca79c8
thanhkk204 <saibattu3kk@gmail.com>
2024-03-19 23:51:58 +0700

    [#9]: Hoàn thiện trang DoctorDetail

---
+++ frontend/src/utils/formatDate.js
@@ -0,0 +1,5 @@
+export const formatDate = (date, config) => {
+  const defaultOption = { day: "numeric", month: "short", year: "numeric" }
+  const options = config ? config : defaultOption
+  return new Date(date).toLocaleDateString("en-US", options)
+}
